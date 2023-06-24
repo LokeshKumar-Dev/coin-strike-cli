84628f274631ded40c981689bf1584bab087cbe0
LokeshKumar-Dev <m.a.lokeshkumar@gmail.com>
2023-06-24 15:05:01 +0530

    full ver

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+import "./index.css";
+
+import Test from "./Test";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Test />
+    {/* <App /> */}
+  </React.StrictMode>
+);
+// @primary-color: #64927c;
+// @background-color: ##1b191b;
+// @font-color: #ffffff;
+// @another-color: #D94E33;
